a311cdbbf01eca6de53d246f5729f699b58ffaad
Monsieur486 <monsieur486@gmail.com>
2020-01-29 17:10:04 +0100

    Mise en Page

--- config/bundles.php
+++ config/bundles.php
@@ -16,5 +16,4 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -100,8 +100,7 @@ class AppAuthenticator extends AbstractFormLoginAuthenticator implements Passwor
             return new RedirectResponse($targetPath);
         }
 
-        // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return $this->urlGenerator->generate('main');
     }
 
     protected function getLoginUrl()
