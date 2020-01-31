d6de2cb00ca0ac571e776dfe2ad8c0696983f13d
Monsieur486 <monsieur486@gmail.com>
2020-01-31 10:17:25 +0100

    Modif Scss

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,18 +2,29 @@
 
 namespace App\Controller;
 
+use App\Repository\ActeurRepository;
+use App\Repository\CauseRepository;
+use App\Repository\DonateurRepository;
+use App\Repository\FavoriActeurRepository;
+use App\Repository\UserRepository;
+use App\Repository\WilderRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
     /**
      * @Route("/", name="main")
+     * @param WilderRepository $wilderRepository
+     * @return Response
      */
-    public function index()
+    public function index(WilderRepository $wilderRepository):Response
     {
+        $wilders = $wilderRepository->findAll();
         return $this->render('main/index.html.twig', [
             'activePage' => 'main',
+            'wilders'=> $wilders
         ]);
     }
 
